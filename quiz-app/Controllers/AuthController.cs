2011f2c1e84c74cac435210b6ea141f198ea1eaa
Maksym Loshadenko <maksym.loshadenko@gmail.com>
2022-12-13 03:39:29 +0200

    Updated auth controller

--- quiz-app/Controllers/AuthController.cs
+++ quiz-app/Controllers/AuthController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using quiz_app.DbMock;
 using quiz_app.Logic;
-using quiz_app.Models.Auth;
 using System.Net;
 using System.Security.Claims;
 using System.Text;
@@ -10,6 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using quiz_app.Models.Web.Responses;
 using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 using Microsoft.AspNetCore.Authorization;
+using quiz_app.Models.Web.Requests;
 
 namespace quiz_app.Controllers
 {
@@ -28,55 +28,31 @@ namespace quiz_app.Controllers
 
         [HttpPost("/auth/sign-in")]
         [AllowAnonymous]
-        public TokenRequestResult SignIn(User user)
+        public ResponseAuthToken SignIn(RequestAuth user)
         {
-            var result = new TokenRequestResult();
+            var result = new ResponseAuthToken();
 
-            var foundUser = Users.GetList().FirstOrDefault(u => u.Username == user.Username);
-
-            if (foundUser == null)
+            try
             {
-                result.SetError("Invalid user credentials.");
+                var foundUser = AuthLogic.GetUserByUsername(user.Username);
+
+                if (!AuthLogic.ComparePassword(user.Password, foundUser.HashedPassword, foundUser.Salt))
+                    throw new Exception("Invalid user credentials.");
+
+                var issuer = _config["Security:Issuer"];
+                var audience = _config["Security:Audience"];
+                var key = Encoding.UTF8.GetBytes(_config["Security:Key"]);
+
+                result.Result.AuthToken = AuthLogic.GenerateJwtToken(user, issuer, audience, key) ??
+                    throw new Exception("Unable to generate JWT tokens");
+            }
+            catch(Exception ex)
+            {
+                result.SetError(ex.GetBaseException().Message);
                 result.Result = null;
 
                 Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-
-                return result;
             }
-
-            if (!Auth.ComparePassword(user.Password, foundUser.HashedPassword, foundUser.Salt))
-            {
-                result.SetError("Invalid user credentials.");
-                result.Result = null;
-
-                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-
-                return result;
-            }
-
-            var issuer = _config["Security:Issuer"];
-            var audience = _config["Security:Audience"];
-            var key = Encoding.UTF8.GetBytes(_config["Security:Key"]);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new[]
-                {
-                    new Claim("Id", Guid.NewGuid().ToString()),
-                    new Claim(JwtRegisteredClaimNames.Sub, user.Username),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                }),
-                Expires = DateTime.UtcNow.AddMinutes(5),
-                Issuer = issuer,
-                Audience = audience,
-                SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature
-                )
-            };
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var jwtToken = tokenHandler.WriteToken(token);
-
-            result.Result.AuthToken = jwtToken;
 
             return result;
         }
